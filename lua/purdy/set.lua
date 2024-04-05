3bfcf8cfe0108e66dabbe4640b377383ba167b62
Michael Purdy <michaeljosephpurdy@gmail.com>
2024-04-05 09:26:23 -0400

    ignore case when searching

--- lua/purdy/set.lua
+++ lua/purdy/set.lua
@@ -1,20 +1,30 @@
+-- global vim
+
+-- line numbers
 vim.opt.nu = true
 
+-- line wrap
+vim.opt.wrap = false
+
+-- spacing
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
-vim.opt.wrap = false
 
+-- swaps and undos
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv('HOME') .. '/.vim/undodir'
 vim.opt.undofile = true
 
+-- searching
 vim.opt.hlsearch = true
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
 vim.opt.termguicolors = true
 
