ac1b890e71fbaf923099feab694f149dc2f37cd3
Michael Purdy <michaeljosephpurdy@gmail.com>
2024-08-07 17:27:13 -0400

    make slight changes to bemol lookup

--- after/plugin/bemol.lua
+++ after/plugin/bemol.lua
@@ -4,17 +4,18 @@ local function bemol()
       return
   end
 
-  local ws_folders_lsp = {}
-    local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
-    if file then
-      for line in file:lines() do
-        table.insert(ws_folders_lsp, line)
-      end
-      file:close()
-    end
+  local ws_folders_lsp = vim.lsp.buf.list_workspace_folders()
 
-  for _, line in ipairs(ws_folders_lsp) do
-    vim.lsp.buf.add_workspace_folder(line)
+  local file = io.open(bemol_dir .. '/ws_root_folders', 'r')
+  if file then
+    for line in file:lines() do
+      table.insert(ws_folders_lsp, line)
+    end
+    file:close()
+  end
+
+  for _, folder in ipairs(ws_folders_lsp) do
+    vim.lsp.buf.add_workspace_folder(folder)
   end
 end
 
